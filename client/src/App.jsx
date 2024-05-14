f6729a42950e305c420c66d2c073d0f297b7ea6a
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-05-14 19:21:08 -0400

    Apollo Provider added for app.jsx,setting up graphql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,32 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+// Import CSS file for styling
+import "./App.css";
 
-import Navbar from './components/Navbar';
+// Import necessary components and modules
+import { Outlet } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
+import Navbar from "./components/Navbar";
+
+// Create a new ApolloClient instance with specified URI and cache
+const client = new ApolloClient({
+  uri: "/graphql", // Endpoint for GraphQL API
+  cache: new InMemoryCache(), // Cache implementation for ApolloClient
+});
+
+// Define the main App component
 function App() {
   return (
     <>
-      <Navbar />
-      <Outlet />
+      {/* Wrap the entire application with ApolloProvider to enable Apollo Client */}
+      <ApolloProvider client={client}>
+        {/* Render the Navbar component */}
+        <Navbar />
+        {/* Render the nested routes */}
+        <Outlet />
+      </ApolloProvider>
     </>
   );
 }
 
+// Export the App component as the default export
 export default App;
