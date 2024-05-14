21aa0883d40ef266cca7f6844066a594bb1ce05b
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2024-05-14 19:26:22 -0400

    Created mutations.js ,Added mutations for login,addUser,SaveBook,RemoveBook

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,104 @@
+// Import gql function from Apollo Client
+import { gql } from "@apollo/client";
+
+// Define GraphQL mutation for logging in a user
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        bookCount
+        email
+        savedBooks {
+          authors
+          bookId
+          description
+          image
+          link
+          title
+        }
+        username
+      }
+    }
+  }
+`;
+
+// Define GraphQL mutation for adding a user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        bookCount
+        email
+        username
+        savedBooks {
+          authors
+          bookId
+          description
+          image
+          link
+          title
+        }
+      }
+    }
+  }
+`;
+
+// Define GraphQL mutation for saving a book
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+    $authors: [String]!
+    $description: String!
+    $title: String!
+    $bookId: String!
+    $image: String!
+    $link: String
+    $userId: String
+  ) {
+    saveBook(
+      authors: $authors
+      description: $description
+      title: $title
+      bookId: $bookId
+      image: $image
+      link: $link
+      userId: $userId
+    ) {
+      _id
+      bookCount
+      email
+      savedBooks {
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+      username
+    }
+  }
+`;
+
+// Define GraphQL mutation for removing a book
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!, $userId: String!) {
+    removeBook(bookId: $bookId, userId: $userId) {
+      _id
+      bookCount
+      email
+      username
+      savedBooks {
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+    }
+  }
+`;
